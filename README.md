39dab4a54fd1b6f0823ebc2b1cd9508f2a9ee73f
dmitriy <kulikov.perm@gmail.com>
2018-04-11 00:58:23 +0300

    Add validations to user and question

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
-# README
+# Аналог сайта ask.fm на Ruby on Rails
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+Создан в учебных целях.
 
-Things you may want to cover:
-
-* Ruby version
-
-* System dependencies
-
-* Configuration
-
-* Database creation
-
-* Database initialization
-
-* How to run the test suite
-
-* Services (job queues, cache servers, search engines, etc.)
-
-* Deployment instructions
-
-* ...
+На данный момент проект в стадии разработки.
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
 
   validates :email, :username, presence: true
   validates :email, :username, uniqueness: true
+  validates :username, length: { maximum: 40 }
+  validates :username, format: {with: /\w/ }
+  validates :email, format: {with: /\A[a-zA-Z\d\.]+@[a-zA-Z\d]*\.+([a-zA-Z\.]{2,})\z/,
+    message: "incorrect email"}
 
   attr_accessor :password
 
@@ -22,8 +26,7 @@ class User < ApplicationRecord
       # создаем т.н. "соль" - рандомная строка усложняющая задачу хакерам
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
 
-      # создаем хэш-пароля - длинная уникальная строка, из которой невозможно восстановить
-      # исходный пароль
+      # создаем хэш-пароля - длинная уникальная строка, из которой невозможно восстановить исходный пароль
       self.password_hash = User.hash_to_string(
         OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
       )
