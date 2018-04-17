a417dc15a2b8d194252525a23ec978e75a1430c0
dmitriy <kulikov.perm@gmail.com>
2018-04-17 22:51:41 +0300

    Users and Sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,58 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :new, :create]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Dima',
-        username: 'dimaon',
-        avatar_url: 'http://digitalspyuk.cdnds.net/17/25/980x490/landscape-1498216547-avatar-neytiri.jpg',
-      ),
-      User.new(
-        id: 2,
-        name: 'Nastya',
-        username: 'chudo',
-        avatar_url: 'http://digitalspyuk.cdnds.net/17/25/980x490/landscape-1498216547-avatar-neytiri.jpg',
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    @user = User.new
+  end
+
+  def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: "Пользователь создан"
+    else
+      render 'new'
+    end
   end
 
   def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Пользователь обновлен"
+    else
+      render 'new'
+    end
   end
 
   def show
-    @user =
-      User.new(
-        name: 'Dima',
-        username: 'dimaon',
-        avatar_url: 'http://digitalspyuk.cdnds.net/17/25/980x490/landscape-1498216547-avatar-neytiri.jpg'
-      )
-
-    @questions = [
-        Question.new(text: "Текст вопроса 1", created_at: Date.parse('27.03.2018')),
-        Question.new(text: "Текст вопроса 1", created_at: Date.parse('27.03.2018')),
-        Question.new(text: "Текст вопроса 1", created_at: Date.parse('27.03.2018')),
-    ]
-
-    @new_question = Question.new
+    @questions = @user.questions.order(created_at: :desc)
+    @new_question = @user.questions.build
   end
+
+  private
+  def user_params
+     params.require(:user).permit(:email, :password, :password_confirmation, :name, :username, :avatar_url)
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
 end
