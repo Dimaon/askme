6242a68d0436a812028c6148ad79501169375cf8
Dimaon <kulikov.perm@gmail.com>
2018-05-31 02:28:19 +0300

    Move hashtag logic to hashtag model

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,8 +4,41 @@ class Hashtag < ApplicationRecord
 
   validates :tag, presence: true
 
-  # Получаем из текста массив тегов
-  def self.text_to_tags_ary(text)
-    text.scan(/#[^\!\?\.\s]+/)
+  # метод наполняющий массив тегами, которые надо удалить
+  # и которые надо добавить/обновить
+  def self.prepare_tags(question)
+    tags_ary = self.tags_to_ary(question.text_with_tags)
+    # Проверяем, есть ли вообще теги в тексте
+    if tags_ary.any?
+      # Проходимся по массиву тегов и проверяем, есть ли тег в таблице тегов и таблице hashtags_questions
+      tags_ary.each do |tag|
+        # Пропускаем иттерацию, если тег уже есть
+        next if question.hashtags.find_by(tag: tag)
+        self.add_tag(question, tag)
+      end
+    else
+      # удаляем все теги, если их вообще не встретилось в тексте
+      self.del_tags(question, tags_ary)
+    end
   end
+
+  private
+  # Получаем из текста массив тегов
+  def self.tags_to_ary(text_with_tags)
+    text_with_tags.scan(/#[^\!\?\.\s]+/)
+  end
+
+  # Добавлем найденный тег или создаем и добавлем тег, если его не было
+  def self.add_tag(question, tag)
+    hash_tag_to_add = self.find_by(tag: tag) || self.create(tag: tag)
+    question.hashtags << hash_tag_to_add
+  end
+
+  # удаляем теги из таблицы hashtags_questions, которых нету в тексте
+  def self.del_tags(question, tags_ary)
+    question.hashtags.each do |t|
+      question.hashtags.delete(t) unless tags_ary.include?(t.tag)
+    end
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -7,41 +7,15 @@ class Question < ApplicationRecord
   validates :text, presence: true
   validates :text, length: { maximum: 255 }
 
-  after_save :prepare_tags
+  after_save :action_with_tags
 
-  private
 
   def text_with_tags
-    text = self.text.to_s + " " + self.answer.to_s
-    Hashtag.text_to_tags_ary(text)
+    self.text.to_s + " " + self.answer.to_s
   end
 
-  def prepare_tags
-    # Проверяем, есть ли вообще теги в тексте
-    if text_with_tags.any?
-      # Проходимся по массиву тегов и проверяем, есть ли тег в таблице тегов и таблице hashtags_questions
-      text_with_tags.each do |tag|
-        # Пропускаем иттерацию, если тег уже есть
-        next if self.hashtags.find_by(tag: tag)
-        add_tag(tag)
-      end
-      del_tags
-    else
-      # удаляем все теги, если их вообще не встретилось в тексте
-      self.hashtags.delete_all
-    end
-  end
-
-  # удаляем теги из таблицы hashtags_questions, которых нету в тексте
-  def del_tags
-    self.hashtags.each do |t|
-      self.hashtags.delete(t) unless text_with_tags.include?(t.tag)
-    end
-  end
-
-  # Добавлем найденный тег или создаем и добавлем тег, если его не было
-  def add_tag(tag)
-    hash_tag = Hashtag.find_by(tag: tag) || Hashtag.create(tag: tag)
-    self.hashtags << hash_tag
+  private
+  def action_with_tags
+    Hashtag.prepare_tags(self)
   end
 end
